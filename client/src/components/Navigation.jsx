4ba14f1c66e5dfdb6425bf1a831b134757c57cb1
ubcall85 <100748920+ubcall85@users.noreply.github.com>
2022-11-14 21:42:20 +0400

    a

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BsArrowBarDown } from 'react-icons/bs';
+
+
+
+const Navigation = () => {
+
+    return (
+        <nav>
+            <div className="groupManagment">
+                <span><BsArrowBarDown />Управление группами</span>
+            </div>
+        </nav>
+    );
+}
+
+export default Navigation;
